d2bd28920f41b8a9701e1064528f8d41b8c002b8
Abeeradil <Aboor.1048@gmail.com>
2025-02-11 14:40:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    // public static class ExtractSubstring {
+    public static void main(String[] args) {
+        Scanner data = new java.util.Scanner(System.in);
+
+////        Q1.Develop a program that takes the weight (in kilograms) and height (in meters)
+////        as input and calculates the BMI, then prints it.
+//
+//        System.out.print("Weight (kg) = ");
+//        double weight =data.nextDouble();
+//
+//        System.out.print("Height (m) = ");
+//        double Height =data.nextDouble();
+//
+//        double BMI=weight/(Height*Height);
+//        System.out.println("BMI = " + BMI);
+
+////        Q2. Write a program that takes the obtained marks and total marks as input and
+////        calculates the percentage, then prints it.
+//        System.out.print("ObtainedMarks= ");
+//        double ObtainedMarks =data.nextDouble();
+//        System.out.print("totalMarks= ");
+//        double totalMarks=data.nextDouble();
+//
+//        System.out.println("Percentage = " + (ObtainedMarks/totalMarks)*100+"%");
+//
+////        Q3. Create a program that takes an amount in one currency and an exchange rate
+////        as input, then converts and prints the amount in another currency.
+//        System.out.print("Amount in USD = ");
+//        double Amount =data.nextDouble();
+//        System.out.print("Exchange Rate (USD to EUR) = ");
+//        double ExchangeRate =data.nextDouble();
+//
+//        System.out.println("Amount in EUR = " + Amount*ExchangeRate);
+
+////        Q4. Create a program that takes a string as input, calculates its length, and then
+////        reverses the string using the StringBuilder class, finally printing both the length and
+////        reversed string.
+//
+//       System.out.println("input: ");
+//       String willcome=data.nextLine();
+//
+//       System.out.print("Length of the string: "+ willcome.length());
+//
+//        StringBuilder sb = new StringBuilder(willcome);
+//        sb.reverse();
+//        System.out.println(" And Reversed string: "+ sb);
+
+////        5. Develop a program that takes a sentence as input and extracts a substring from
+////        it, then prints the extracted substring.
+//
+//        System.out.print("Input: Sentence =  ");
+//        String sentence = data.nextLine();
+//
+//        System.out.print(", Start Index = ");
+//        int startIndex = data.nextInt();
+//
+//        System.out.print(", End Index = ");
+//        int endIndex = data.nextInt();
+//
+//        if (startIndex>= 0 && endIndex <= sentence.length() && startIndex < endIndex) {
+//            String extracted= sentence.substring(startIndex, endIndex).trim();
+//            System.out.println("Expected Output: " + extracted);
+//        } else {
+//
+//            System.out.println("Error!!!");
+//
+//        }
+//
+////        Q6. Write a program that takes a sentence and a keyword as input, then check if
+////        the keyword is present in the sentence and prints the result.
+//
+//        System.out.print("Input: Sentence =  ");
+//        String sentence = data.nextLine();
+//
+//        System.out.print(", Keyword = ");
+//        String keyWord = data.nextLine();
+//
+//        if (isWordPresent( sentence, keyWord)) {
+//            System.out.print("keyWord\"" + keyWord + "\" is present in the sentence.");
+//        }else {
+//            System.out.print("keyWord\"" +keyWord+"\" is NOT present in the sentence.");
+//
+//    }
+//    }
+//
+//    static boolean isWordPresent(String sentence, String keyWord) {
+//        String[] words = sentence.toLowerCase().split("");
+//        keyWord=keyWord.toLowerCase();
+//        for (String word : words) {
+//            if (word.equals(keyWord)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    }
+////       Q7.Develop a program that takes a sentence and a word to replace as input, then
+////       replace all occurrences of the word with another word and prints the modified
+////       sentence.
+//
+//        System.out.print("Input: Sentence =  ");
+//        String sentence = data.nextLine();
+//        System.out.print(", Word to Replace = ");
+//        String oldWord = data.nextLine();
+//        System.out.print(", Replacement Word = ");
+//        String newWorla = data.nextLine();
+
+//         String modifiedSentence= sentence.replaceAll("\\b"+oldWord+"\\b",newWorla);
+//        System.out.println("Expected Output: "+modifiedSentence);
+//
+////        Q8. Write a program that takes two strings as input and check if they are equal,
+////                ignoring the case, then prints whether they are equal or not.
+//        System.out.print("input: String 1 = ");
+//        String str1 = data.nextLine();
+//
+//
+//        System.out.print(", String 2 = ");
+//        String str2 = data.nextLine();
+//
+//        if (str1.equalsIgnoreCase(str2)) {
+//            System.out.println("Strings are equal (ignoring case).");
+//        } else {
+//            System.out.println("Strings are NOT equal.");
+//        }
+
+    }
+}
+
+
+
+
+
+
+
